1db54295df4a8ee174ecd75c6c98c4b7fb5b8cb9
Thedrunkenbrick <48966338+drunkenbrick@users.noreply.github.com>
2021-09-14 15:58:31 +1200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+readyToInstall=no
+
+#Installing required programs
+
+
+while [[ "$readyToInstall" != "Y" ]] 
+do
+	clear
+	#Getting the user options
+	echo "-----WELCOME-----"
+	read -p "Would you like to install the base programs [Y/n] " basePrograms
+	read -p "Would you like to copy the config files [Y/n] " copyFiles
+
+
+	
+	#Setting the option to yes if no option is selected
+	if [ -z $basePrograms ]
+	then 
+		basePrograms="Y"
+	fi
+
+	if [ -z $copyFiles ]
+	then 
+		copyFiles="Y"
+	fi
+
+	
+  	#Confirming the options of the user
+	printf "\n"
+	echo "-----OPTIONS-----"
+	echo "Base programs: $basePrograms"
+	echo "Copy files: $copyFiles"	
+	
+	read -p "Are these the settings you would like	[Y/n] " readyToInstall
+	
+	#Have to do this cuz cant figure out how to do or statement in while loop
+	if [ -z $readyToInstall ] || [ "$readyToInstall" == 'y' ]
+	then
+		readyToInstall="Y"
+	fi
+
+
+done
+
+clear
+
+#Installing the base programs required for the setup
+if [ "$basePrograms" == "y" ] || [ "$basePrograms" == 'Y' ] 
+then 
+	echo "Installing base programs"
+	sudo pacman -S bspwm dunst kitty polybar rofi sxhkd zathura ranger
+
+elif [ "$basePrograms" == "n" ] || [ "$basePrograms" == 'N' ]
+then
+	echo "Skipping install of base programs"
+
+else
+	echo "Invalid option"
+fi
+
+#Copying the config files to the correct locations
+if [ "$copyFiles" == 'y' ] || [ "$copyFiles" == 'Y' ]
+then 
+	echo "Installing configs"
+	sudo cp -r BaseDots/bspwm ~/.config
+	sudo cp -r BaseDots/dunst ~/.config
+	sudo cp -r BaseDots/kitty ~/.config
+	sudo cp -r BaseDots/polybar ~/.config
+	sudo cp -r BaseDots/rofi ~/.config
+	sudo cp -r BaseDots/sxhkd ~/.config
+	sudo cp -r BaseDots/zathura ~/.config
+	sudo cp BaseDots/.bashrc ~
+	sudo cp -r BaseDots/ranger ~/.config
+	echo "Copied succesfully"
+
+elif [ "$copyFiles" == "n" ] || [ "$copyFiles" == 'N' ]
+then
+	echo "Skipping config installation"
+
+
+else
+	echo "Invalid option"
+fi
